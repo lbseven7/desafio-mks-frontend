1eb25fe485124107fbfa3778d3449d75c05d589e
lbseven7 <leobarbosapinturas@gmail.com>
2023-05-24 21:02:29 -0300

    feat: adiciona ul para exibir os produtos no checkout (não funcionou)

--- src/pages/checkout/Checkout.tsx
+++ src/pages/checkout/Checkout.tsx
@@ -1,14 +1,26 @@
+import React, { useContext } from "react";
 import Header from "../../components/header/Header";
+import { CartContext } from "../../CartContext";
+import { Product } from "../../interfaces/Product";
 
 function Checkout() {
+  const { cartItems } = useContext(CartContext);
+
   return (
     <>
       <Header />
       <div>
         <h1>Checkout</h1>
+        <ul>
+          {cartItems.map((item: Product) => (
+            <li key={item.id}>{item.name}</li>
+          ))}
+        </ul>
       </div>
     </>
   );
 }
 
 export default Checkout;
+
+
